3719128e9a47851bce0e43c911220236b369b039
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-03-20 21:39:37 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,69 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zalihe
+namespace Prvenstvo
 {
     internal class Program
-    { 
-        static Proizvod proizvod = new Proizvod();
-        static Skladiste skladiste = new Skladiste();
+    {
+        static Prvenstvo prvenstvo = new Prvenstvo();
         static void Main(string[] args)
         {
-            int odabir = 0;
+            Console.WriteLine("PRVENSTVO APP");
+            int odabir;
+          
             do
             {
-                List<Proizvod> SviProizvodi = new List<Proizvod>();
-                Console.WriteLine("Odaberite");
-                Console.WriteLine("[1] Dodaj proizvod na skladište");
-                Console.WriteLine("[2] Povećaj količinu proizvoda");
-                Console.WriteLine("[3] Smanji količinu proizvoda");
-                Console.WriteLine("[4] Ispiši stanje skladišta");
-                Proizvod pro = new Proizvod();
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("[1] Unesi reprezentaciju");
+                Console.WriteLine("[2] Evidentiraj utakmicu");
+                Console.WriteLine("[3] Ispiši tablicu rezultata");
+                Console.WriteLine("[4] Ispiši bodovnu ljestvicu");
+                Console.WriteLine("[0] Izlaz");
+                Console.WriteLine("------------------------------");
                 odabir = int.Parse(Console.ReadLine());
-                if(odabir != 5)
+                string oznaka;
+                string naziv;
+                string domacin, gost;
+                int domacinZgoditci, gostZgoditci;
+                
+                switch (odabir)
                 {
-                    switch (odabir)
-                    {
-                        case 1:
-                            Console.WriteLine("Unesite naziv proizvoda ");
-                            pro.Naziv = Console.ReadLine();
-                            Console.WriteLine("Unesite cijenu proizvoda ");
-                            pro.Cijena = double.Parse(Console.ReadLine());
-                            Console.WriteLine("Unesite stanje proizvoda ");
-                            pro.Stanje = int.Parse(Console.ReadLine());
-                            skladiste.DodajProizvod(pro);
-                            break;
-                        case 2:
-                            Console.WriteLine("Unesite naziv proizvoda ");
-                            string naziv = Console.ReadLine();
-                            Proizvod povecaniProizvod=skladiste.DohvatiProizvod(naziv);
-                            Console.WriteLine("Unesite količinu proizvoda ");
-                            int kolicina = int.Parse((Console.ReadLine()));
-                            povecaniProizvod.DodajNaStanje(kolicina);
-                            break;
-                        case 3:
-                            Console.WriteLine("Unesite naziv proizvoda ");
-                            string snaziv = Console.ReadLine();
-                            Proizvod smanjeniProizvod = skladiste.DohvatiProizvod(snaziv);
-                            Console.WriteLine("Unesite količinu za koju želite smanjiti stanje");
-                            int kol = int.Parse((Console.ReadLine()));
-                            smanjeniProizvod.OduzmiSaStanja(kol);
-                            break;
+                    case 1:
+                        Console.Write("Unesite oznaku ");
+                        oznaka = Console.ReadLine();
+                        Console.Write("Unesite naziv reprezentacije ");
+                        naziv = Console.ReadLine();
+                        prvenstvo.DodajReprezentaciju(oznaka, naziv);
+                        break;
 
-                        case 4:
-                            SviProizvodi = skladiste.DohvatiSveProizvode();
-                            double ukupno = skladiste.IzracunajUkupnuVrijednostZaliha();
-                            foreach (Proizvod p in SviProizvodi)
-                            {
-                                Console.WriteLine($"Proizvod: {p.Naziv}, Cijena: {p.Cijena}, Stanje: {p.Stanje}");
-                            }
-                            Console.WriteLine($"Ukupna vrijednost svih proizvoda je {ukupno} ");
-                            break;
-                    }
-
+                    case 2:
+                        Console.Write("Unesite domacina ");
+                        domacin = Console.ReadLine();
+                        Console.Write("Unesite gosta ");
+                        gost = Console.ReadLine();
+                        Console.Write("Zgoditci domacina ");
+                        domacinZgoditci = int.Parse(Console.ReadLine());
+                        Console.Write("Zgoditci gosta ");
+                        gostZgoditci = int.Parse(Console.ReadLine());
+                        prvenstvo.DodajUtakmicu(domacin, gost, domacinZgoditci, gostZgoditci);
+                        break;
+                    case 3:
+                        prvenstvo.GenerirajTablicuRezultata();
+                        break;
+                    case 4:
+                        prvenstvo.GenerirajBodovnuLjestvicu();
+                        break;
                 }
-            } while (odabir != 5);
-            Console.ReadLine();
+            } while (odabir != 0);
         }
     }
 }
