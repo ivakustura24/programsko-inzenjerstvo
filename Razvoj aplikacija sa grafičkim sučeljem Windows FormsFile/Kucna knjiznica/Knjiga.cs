241e317df85a721e6b325c61565b682347c685d7
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-01 23:44:18 +0200

    Add files via upload

---
+++ Razvoj aplikacija sa grafičkim sučeljem Windows FormsFile/Kucna knjiznica/Knjiga.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kucna_knjiznica_2
+{
+    internal class Knjiga
+    {
+        public string Naslov { get; set; }
+        public bool Dostupno { get; set; }
+        public string Osoba { get; set; }
+        public Knjiga() { }
+        public Knjiga (string naslov)
+        {
+            Naslov = naslov;
+            Dostupno = true;
+            Osoba = "";
+
+        }
+        public string DohvatiOpis()
+        {
+            string ispis = "Naslov:" + Naslov + ", Dostupno:" + Dostupno + ", Osoba:" + Osoba;
+            return ispis;
+        }
+    }
+}
---
+++ Razvoj aplikacija sa grafičkim sučeljem Windows FormsFile/Kucna knjiznica/Knjiznica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kucna_knjiznica_2
+{
+    internal class Knjiznica
+    {
+        public List<Knjiga> PopisKnjiga;
+        public Knjiznica()
+        {
+            PopisKnjiga = new List<Knjiga>();
+            PopisKnjiga.Add(new Knjiga("Zlocin i kazna"));
+            PopisKnjiga.Add(new Knjiga("Ana Karenjina"));
+            PopisKnjiga.Add(new Knjiga("Rat i mir"));
+
+        }
+        public void Posudi(string ime, string naslov)
+        {
+            foreach (Knjiga k in PopisKnjiga)
+            {
+                if (k.Naslov == naslov)
+                {
+                    k.Dostupno = false;
+                    k.Osoba = ime;
+                }
+            }
+        }
+    }
+}
