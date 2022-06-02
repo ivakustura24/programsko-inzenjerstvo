6ab13428fff9dff58fef13ee5e57c54080d710ff
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-03 00:57:09 +0200

    Add files via upload

---
+++ Principi objektno orijentiranog programiranja/Prijava u sustav/Autentifikator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prijava_u_sustav
+{
+    internal class Autentifikator
+    {
+        private List<Korisnik> listaKorisnika; 
+        public Autentifikator()
+        {
+            listaKorisnika = new List<Korisnik>();
+            listaKorisnika.Add(new Korisnik("pperic", "1111"));
+            listaKorisnika.Add(new Korisnik("ikustura", "2404"));
+            listaKorisnika.Add(new Korisnik("mcolle", "2807"));
+
+        }
+        private Korisnik DohvatiKorisnika (string korisnickoIme)
+        {
+            Korisnik trazeni = null;
+            foreach (Korisnik k in listaKorisnika)
+            {
+                if(k.KorisnickoIme == korisnickoIme)
+                {
+                    trazeni = k;
+                }
+            }
+            return trazeni;
+        }
+        public string PrijaviKorisnika (string korisnickoIme, string lozinka)
+        {
+            Korisnik prijava = DohvatiKorisnika(korisnickoIme);
+            if (prijava != null && prijava.Lozinka== lozinka)
+            {
+                return "Uspješno prijavljen korisnik!";
+            }
+            else if (prijava!=null && prijava.Lozinka != lozinka)
+            {
+                return "Pogrešno upisana lozinka!";
+
+            }
+            else
+            {
+                return "Ne postoji korisnik sa navedenim korisničkim imenon!";
+            }
+        }
+    }
+}
---
+++ Principi objektno orijentiranog programiranja/Prijava u sustav/Korisnik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prijava_u_sustav
+{
+    internal class Korisnik
+    {
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public Korisnik( string korisnickoIme, string lozinka)
+        {
+            KorisnickoIme = korisnickoIme;
+            Lozinka = lozinka;
+        }
+    }
+}
