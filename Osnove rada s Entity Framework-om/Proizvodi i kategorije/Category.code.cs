471db8b4f91e22fd35dfe4308ecb8482ec5391ca
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-02 22:38:35 +0200

    Add files via upload

---
+++ Osnove rada s Entity Framework-om/Proizvodi i kategorije/Category.code.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Products_and_categories
+{
+    public partial class Category
+    {
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Osnove rada s Entity Framework-om/Proizvodi i kategorije/products.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09_Products_and_categories
+{
+    public partial class productsFrm : Form
+    {
+        public productsFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void productsFrm_Load(object sender, EventArgs e)
+        {
+            Osvjezi();
+        }
+
+        private void Osvjezi()
+        {
+            dgvProducts.DataSource = GetProducts();
+            dgvProducts.Columns["Category"].Visible = false;
+        }
+
+        private object GetProducts()
+        {
+            using (var context = new EF_DBEntities())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            AddProduct forma = new AddProduct();
+            forma.ShowDialog();
+            Osvjezi();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using(var context = new EF_DBEntities())
+            {
+                Product selectedProduct = dgvProducts.CurrentRow.DataBoundItem as Product;
+                context.Products.Attach(selectedProduct);
+                context.Products.Remove (selectedProduct);
+                context.SaveChanges();
+            }
+            Osvjezi();
+        }
+    }
+}
