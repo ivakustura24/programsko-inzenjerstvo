68051123e3e91d8a6f88c80527c89a29f7c3b36a
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-02 22:34:47 +0200

    Add files via upload

---
+++ Osnove rada s Entity Framework-om/Zadatci/AssignmentResult.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace _07_Assingments
+{
+    partial class AssignmentResultFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvStatuses = new System.Windows.Forms.DataGridView();
+            this.dgvAssignments = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbStudent = new System.Windows.Forms.TextBox();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbPoints = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStatuses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAssignments)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Statuses";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Assignments";
+            // 
+            // dgvStatuses
+            // 
+            this.dgvStatuses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStatuses.Location = new System.Drawing.Point(25, 44);
+            this.dgvStatuses.Name = "dgvStatuses";
+            this.dgvStatuses.RowHeadersWidth = 51;
+            this.dgvStatuses.RowTemplate.Height = 24;
+            this.dgvStatuses.Size = new System.Drawing.Size(330, 125);
+            this.dgvStatuses.TabIndex = 2;
+            this.dgvStatuses.SelectionChanged += new System.EventHandler(this.dgvStatuses_SelectionChanged);
+            // 
+            // dgvAssignments
+            // 
+            this.dgvAssignments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAssignments.Location = new System.Drawing.Point(25, 195);
+            this.dgvAssignments.Name = "dgvAssignments";
+            this.dgvAssignments.RowHeadersWidth = 51;
+            this.dgvAssignments.RowTemplate.Height = 24;
+            this.dgvAssignments.Size = new System.Drawing.Size(669, 150);
+            this.dgvAssignments.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbPoints);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbDescription);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tbStudent);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(332, 351);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 123);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter assignment result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Student";
+            // 
+            // tbStudent
+            // 
+            this.tbStudent.Location = new System.Drawing.Point(27, 63);
+            this.tbStudent.Name = "tbStudent";
+            this.tbStudent.Size = new System.Drawing.Size(100, 22);
+            this.tbStudent.TabIndex = 1;
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(133, 63);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(100, 22);
+            this.tbDescription.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(130, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Description";
+            // 
+            // tbPoints
+            // 
+            this.tbPoints.Location = new System.Drawing.Point(239, 63);
+            this.tbPoints.Name = "tbPoints";
+            this.tbPoints.Size = new System.Drawing.Size(100, 22);
+            this.tbPoints.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(236, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Points";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(602, 480);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(92, 31);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(504, 480);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(92, 31);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // AssignmentResultFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 528);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvAssignments);
+            this.Controls.Add(this.dgvStatuses);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AssignmentResultFrm";
+            this.Text = "Assignment Result";
+            this.Load += new System.EventHandler(this.AssignmentResultFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStatuses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAssignments)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvStatuses;
+        private System.Windows.Forms.DataGridView dgvAssignments;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbPoints;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbStudent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Osnove rada s Entity Framework-om/Zadatci/AssignmentResult.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_Assingments
+{
+    public partial class AssignmentResultFrm : Form
+    {
+        public AssignmentResultFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void AssignmentResultFrm_Load(object sender, EventArgs e)
+        {
+            dgvStatuses.DataSource = GetStatuses();
+            dgvAssignments.DataSource = GetAssignments();
+            dgvAssignments.Columns["AssignmentStatus"].Visible = false;
+        }
+
+        private object GetAssignments()
+        {
+            using (var context = new EF_DBEntities())
+            {
+                return context.Assignments.ToList();
+            }
+            
+
+        }
+
+        private object GetStatuses()
+        {
+            using (var context = new EF_DBEntities())
+            {
+                return context.AssignmentStatuses.ToList();
+            }
+        }
+        private void Osvjezi()
+        {
+            AssignmentStatus status = GetStatus();
+            dgvAssignments.DataSource = GetAssignments(status);
+        }
+        private void dgvStatuses_SelectionChanged(object sender, EventArgs e)
+        {
+            Osvjezi();
+        }
+
+        private AssignmentStatus GetStatus()
+        {
+            return dgvStatuses.CurrentRow.DataBoundItem as AssignmentStatus;
+        }
+
+        private object GetAssignments(AssignmentStatus status)
+        {
+            using (var context = new EF_DBEntities())
+            {
+                context.AssignmentStatuses.Attach(status);
+                return status.Assignments.ToList();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (var context = new EF_DBEntities())
+            {
+                string student = tbStudent.Text;
+                string description = tbDescription.Text;
+                int point = int.Parse(tbPoints.Text.ToString());
+                AssignmentStatus status = dgvStatuses.CurrentRow.DataBoundItem as AssignmentStatus;
+                context.AssignmentStatuses.Attach(status);
+
+                Assignment assignment = new Assignment()
+                {
+                    Student = student,
+                    Description = description,
+                    Points = point,
+                    IdAssignmentStatuses = status.Id
+                };
+
+                context.Assignments.Add(assignment);
+                context.SaveChanges();
+            }
+            Osvjezi();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Assignment assignment = GetAssignment();
+            using (var context = new EF_DBEntities())
+            {
+                context.Assignments.Attach(assignment);
+                context.Assignments.Remove(assignment);
+                context.SaveChanges();
+            }
+            Osvjezi();
+        }
+
+        private Assignment GetAssignment()
+        {
+           return dgvAssignments.CurrentRow.DataBoundItem as Assignment;
+        }
+    }
+}
