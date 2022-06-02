fba3eb7493a8db5b2eecb917d31c7837e3ce98a5
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-03 00:53:35 +0200

    Add files via upload

---
+++ Principi objektno orijentiranog programiranja/Kucna knjiznica 3/Knjiznica.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kucna_knjiznica_3
+{
+    internal class Knjiznica
+    {
+        public List <Knjiga> Knjige;
+        public List <Posudba> Posudbe;
+
+        public Knjiznica()
+        {
+            Knjige = new List<Knjiga>();
+            Knjige.Add(new Knjiga("1111", "Rat i mir"));
+            Knjige.Add(new Knjiga("2222", "Ana Karenjina"));
+            Knjige.Add(new Knjiga("3333", "Zlocin i kazna"));
+        }
+        public void PosudiKnjigu (string osoba, string isbn)
+        {
+            Posudbe = new List<Posudba>();
+            Knjiga stara = new Knjiga();
+            foreach (Knjiga k in Knjige)
+            {
+                if(k.ISBN == isbn)
+                {
+                    stara = k;
+                    break;
+                    
+                }
+            }
+            Posudbe.Add(new Posudba(osoba, stara));
+      
+        }
+
+
+    }
+}
---
+++ Principi objektno orijentiranog programiranja/Kucna knjiznica 3/Posudba.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kucna_knjiznica_3
+{
+    internal class Posudba
+    {
+        public string Osoba { get; set; }
+        public Knjiga PosudjenaKnjiga { get; set; }
+
+        public DateTime Datum { get; set; }
+        public Posudba(string osoba, Knjiga posudjenaKnjiga)
+        {
+            Osoba = osoba;
+            PosudjenaKnjiga = posudjenaKnjiga;
+            Datum = DateTime.Now;
+               
+        }
+    }
+}
