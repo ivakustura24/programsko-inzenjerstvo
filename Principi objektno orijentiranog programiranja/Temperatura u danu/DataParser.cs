a7541fefa016a54d393f627f2d685404b3dc789a
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-03 00:59:43 +0200

    Add files via upload

---
+++ Principi objektno orijentiranog programiranja/Temperatura u danu/DataParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura_u_danu
+{
+    internal class DataParser
+    {
+        public DataParser() { }
+        static Sensor sensor = new Sensor();
+        public List <double> Parse(string sensorData)
+        {
+            List<double> data = new List<double>();
+            string[] polje=sensor.GetData().Split(';');
+            foreach (string s in polje)
+            {
+                double d = double.Parse(s);
+                data.Add(d);
+            }
+            return data;
+        }
+    }
+}
---
+++ Principi objektno orijentiranog programiranja/Temperatura u danu/Reporter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura_u_danu
+{
+    
+    internal class Reporter
+    {
+        static DataParser data = new DataParser();
+        static Sensor sensor = new Sensor();
+        private double GetMinimalTemperature()
+        {
+            double min=0;
+            List <double> listamin =data.Parse(sensor.GetData());
+            min = listamin.Min();
+            return min;
+        }
+        private double GetMaximalTemperature()
+        {
+            double max = 0;
+            List<double> listamax = data.Parse(sensor.GetData());
+            max = listamax.Max();
+            return max;
+        }
+        private double GetAverageTemperature()
+        {
+            double average = 0;
+            List<double> listaavg=data.Parse(sensor.GetData());
+            average = listaavg.Average();
+            return average;
+        }
+        public void GenerateReport()
+        {
+            Console.WriteLine($"Average temperature is {GetAverageTemperature()} °C, minimale temperature is {GetMinimalTemperature()} °C, maximal temperature is {GetMaximalTemperature()} °C ");
+        }
+    }
+}
