1cd1352db5e6a6bc2850c6ff96119598bb8ff0a2
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-02 18:52:36 +0200

    Add files via upload

---
+++ UML dijagrami aktivnosti i slijeda/Sigurnosna vrata/Repozitorij.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigurnosna_vrata
+{
+    internal class Repozitorij
+    {
+        private List<Korisnik> Korisnici = new List<Korisnik>()
+        {
+            new Korisnik(10001,1234,1) {BrojKartice =10001, PIN = 1234, RazinaPrava = 1},
+            new Korisnik(10001,1234,1) {BrojKartice =10002, PIN = 0000, RazinaPrava = 2},
+            new Korisnik(10001,1234,1) {BrojKartice =10003, PIN = 9999, RazinaPrava = 2},
+
+
+        };
+
+        private List<Vrata> SvaVrata = new List<Vrata>()
+        {
+            new Vrata (501,1) {BrojVrata=501, RazinaPrava=1},
+            new Vrata (501,1) {BrojVrata=502, RazinaPrava=2},
+            new Vrata (501,1) {BrojVrata=503, RazinaPrava=2},
+        };
+
+
+        public Korisnik DohvatiKorisnika (int brKartice, int pin)
+        {
+            Korisnik trazeni = null;
+            foreach(Korisnik k in Korisnici)
+            {
+                if (k.BrojKartice == brKartice && k.PIN == pin)
+                    trazeni = k;
+            }
+            return trazeni;
+        }
+
+        public Vrata DohvatiVrata (int brVrata)
+        {
+             Vrata trazenaVrata = null;
+             foreach (Vrata v in SvaVrata)
+            {
+                if (v.BrojVrata == brVrata)
+                    trazenaVrata = v;
+            }
+            return trazenaVrata;
+        }
+    }
+}
---
+++ UML dijagrami aktivnosti i slijeda/Sigurnosna vrata/Sigurnosna Kontrola.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigurnosna_vrata
+{
+    internal class Sigurnosna_Kontrola
+    {
+        Repozitorij repozitorij = new Repozitorij();
+        public bool OtvoriVrata(int brKartice, int pin, int brVrata)
+        {
+            Repozitorij repozitorij = new Repozitorij();
+            bool prolaz = false;
+            Korisnik korisnik = repozitorij.DohvatiKorisnika(brKartice, pin);
+            Vrata vrata = repozitorij.DohvatiVrata(brVrata);
+            if (korisnik.RazinaPrava >= vrata.RazinaPrava)
+            {
+                prolaz = true;
+            }
+            return prolaz;
+        }
+    }
+}
