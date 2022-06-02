0d86cb497fa5b6671917c4689cb8b6af921f9667
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-02 22:33:23 +0200

    Add files via upload

---
+++ Osnove rada s Entity Framework-om/Zanrovi knjiga 2/Books.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace _06_Zanrovi_knjiga
+{
+    partial class BooksFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.tbOfPages = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMainAuthor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbGenre = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Location = new System.Drawing.Point(27, 26);
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.RowHeadersWidth = 51;
+            this.dgvBooks.RowTemplate.Height = 24;
+            this.dgvBooks.Size = new System.Drawing.Size(758, 329);
+            this.dgvBooks.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title";
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(27, 407);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(100, 22);
+            this.tbTitle.TabIndex = 2;
+            // 
+            // tbOfPages
+            // 
+            this.tbOfPages.Location = new System.Drawing.Point(167, 407);
+            this.tbOfPages.Name = "tbOfPages";
+            this.tbOfPages.Size = new System.Drawing.Size(100, 22);
+            this.tbOfPages.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 379);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "# of Pages";
+            // 
+            // tbMainAuthor
+            // 
+            this.tbMainAuthor.Location = new System.Drawing.Point(295, 407);
+            this.tbMainAuthor.Name = "tbMainAuthor";
+            this.tbMainAuthor.Size = new System.Drawing.Size(100, 22);
+            this.tbMainAuthor.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(292, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Main author";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(428, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Genre";
+            // 
+            // cmbGenre
+            // 
+            this.cmbGenre.FormattingEnabled = true;
+            this.cmbGenre.Location = new System.Drawing.Point(431, 405);
+            this.cmbGenre.Name = "cmbGenre";
+            this.cmbGenre.Size = new System.Drawing.Size(121, 24);
+            this.cmbGenre.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(581, 405);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(99, 23);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(686, 405);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(99, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // BooksFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cmbGenre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbMainAuthor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbOfPages);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvBooks);
+            this.Name = "BooksFrm";
+            this.Text = "Books";
+            this.Load += new System.EventHandler(this.BooksFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.TextBox tbOfPages;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbMainAuthor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbGenre;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Osnove rada s Entity Framework-om/Zanrovi knjiga 2/Books.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06_Zanrovi_knjiga
+{
+    public partial class BooksFrm : Form
+    {
+        public BooksFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void BooksFrm_Load(object sender, EventArgs e)
+        {
+            dgvBooks.DataSource = GetBooks();
+            dgvBooks.Columns["Genre"].Visible = false;
+            cmbGenre.DataSource = GetGenres();
+        }
+
+        private object GetGenres()
+        {
+            using (var context = new EF_DBEntities())
+            {
+                return context.Genres.ToList();
+            }
+        }
+
+        private object GetBooks()
+        {
+            using (var context = new EF_DBEntities())
+            {
+                return context.Books.ToList();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (var context = new EF_DBEntities())
+            {
+                string title = tbTitle.Text;
+                int number = int.Parse(tbOfPages.Text.ToString());
+                string author = tbMainAuthor.Text;
+                Genre genre = cmbGenre.SelectedItem as Genre;
+                context.Genres.Attach(genre);
+
+                Book book = new Book()
+                {
+                    Title = title,
+                    NumOfPages = number,
+                    MainAuthor = author,
+                    IdGenres = genre.Id
+                };
+                context.Books.Add(book);
+                context.SaveChanges();
+            }
+            dgvBooks.DataSource = GetBooks();
+
+            tbMainAuthor.Clear();
+            tbOfPages.Clear();
+            tbTitle.Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (var context = new EF_DBEntities())
+            {
+                Book book = dgvBooks.CurrentRow.DataBoundItem as Book;
+                context.Books.Attach(book); 
+                context.Books.Remove(book);
+                context.SaveChanges();
+            }
+            dgvBooks.DataSource = GetBooks();
+        }
+    }
+}
