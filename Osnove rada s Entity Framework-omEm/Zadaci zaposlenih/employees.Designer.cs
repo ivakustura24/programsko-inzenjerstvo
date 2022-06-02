e191bbb46ef70190daf9e18e5c8d5141370a9905
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-02 22:41:04 +0200

    Add files via upload

---
+++ Osnove rada s Entity Framework-omEm/Zadaci zaposlenih/employees.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace _11_EmployeesTasks
+{
+    partial class TasksFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
+            this.dgvTasks = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbPriority = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAddEmployee = new System.Windows.Forms.Button();
+            this.btnDeleteTask = new System.Windows.Forms.Button();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTasks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Location = new System.Drawing.Point(12, 32);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.RowHeadersWidth = 51;
+            this.dgvEmployees.RowTemplate.Height = 24;
+            this.dgvEmployees.Size = new System.Drawing.Size(614, 150);
+            this.dgvEmployees.TabIndex = 0;
+            this.dgvEmployees.SelectionChanged += new System.EventHandler(this.dgvEmployees_SelectionChanged);
+            // 
+            // dgvTasks
+            // 
+            this.dgvTasks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTasks.Location = new System.Drawing.Point(12, 219);
+            this.dgvTasks.Name = "dgvTasks";
+            this.dgvTasks.RowHeadersWidth = 51;
+            this.dgvTasks.RowTemplate.Height = 24;
+            this.dgvTasks.Size = new System.Drawing.Size(614, 150);
+            this.dgvTasks.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 401);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First name:";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(12, 431);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(132, 22);
+            this.tbFirstName.TabIndex = 3;
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(12, 500);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(132, 22);
+            this.tbDescription.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 470);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Description";
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(171, 431);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(132, 22);
+            this.tbLastName.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(171, 401);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Last name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 470);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Priority";
+            // 
+            // cmbPriority
+            // 
+            this.cmbPriority.FormattingEnabled = true;
+            this.cmbPriority.Location = new System.Drawing.Point(171, 500);
+            this.cmbPriority.Name = "cmbPriority";
+            this.cmbPriority.Size = new System.Drawing.Size(132, 24);
+            this.cmbPriority.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Employees";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Tasks";
+            // 
+            // btnAddEmployee
+            // 
+            this.btnAddEmployee.Location = new System.Drawing.Point(359, 431);
+            this.btnAddEmployee.Name = "btnAddEmployee";
+            this.btnAddEmployee.Size = new System.Drawing.Size(134, 23);
+            this.btnAddEmployee.TabIndex = 12;
+            this.btnAddEmployee.Text = "Add employee";
+            this.btnAddEmployee.UseVisualStyleBackColor = true;
+            this.btnAddEmployee.Click += new System.EventHandler(this.btnAddEmployee_Click);
+            // 
+            // btnDeleteTask
+            // 
+            this.btnDeleteTask.Location = new System.Drawing.Point(500, 499);
+            this.btnDeleteTask.Name = "btnDeleteTask";
+            this.btnDeleteTask.Size = new System.Drawing.Size(126, 23);
+            this.btnDeleteTask.TabIndex = 13;
+            this.btnDeleteTask.Text = "Delete task";
+            this.btnDeleteTask.UseVisualStyleBackColor = true;
+            this.btnDeleteTask.Click += new System.EventHandler(this.btnDeleteTask_Click);
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Location = new System.Drawing.Point(359, 499);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(134, 23);
+            this.btnAddTask.TabIndex = 14;
+            this.btnAddTask.Text = "Add task";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // TasksFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 558);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.btnDeleteTask);
+            this.Controls.Add(this.btnAddEmployee);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbPriority);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbDescription);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvTasks);
+            this.Controls.Add(this.dgvEmployees);
+            this.Name = "TasksFrm";
+            this.Text = "Tasks";
+            this.Load += new System.EventHandler(this.TasksFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTasks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEmployees;
+        private System.Windows.Forms.DataGridView dgvTasks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbPriority;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAddEmployee;
+        private System.Windows.Forms.Button btnDeleteTask;
+        private System.Windows.Forms.Button btnAddTask;
+    }
+}
+
---
+++ Osnove rada s Entity Framework-omEm/Zadaci zaposlenih/employees.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _11_EmployeesTasks
+{
+    public partial class TasksFrm : Form
+    {
+        public TasksFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void TasksFrm_Load(object sender, EventArgs e)
+        {
+            dgvEmployees.DataSource = GetEmployees();
+            dgvEmployees.Columns["Tasks"].Visible = false;
+
+            dgvTasks.DataSource = GetTasks();
+            dgvTasks.Columns["Employee"].Visible = false;
+
+            List <int > priority = new List<int>()
+            {
+                1,2,3,4,5
+            };
+
+            cmbPriority.DataSource = priority;
+        }
+
+        private object GetTasks()
+        {
+            using (var context = new EF_DBEntities())
+            {
+                return context.Tasks.ToList();
+            }
+        }
+
+        private object GetEmployees()
+        {
+            using (var context = new EF_DBEntities())
+            {
+                return context.Employees.ToList();
+            }
+        }
+        private void Osvjezi()
+        {
+            
+            Employee selectedEmployee = GetEmployee();
+            dgvTasks.DataSource = GetEmployeesTasks(selectedEmployee);
+        }
+        private void dgvEmployees_SelectionChanged(object sender, EventArgs e)
+        {
+            Osvjezi();
+        }
+
+        private object GetEmployeesTasks(Employee selectedEmployee)
+        {
+            using (var context = new EF_DBEntities())
+            {
+                context.Employees.Attach(selectedEmployee);
+                return selectedEmployee.Tasks.ToList();
+            }
+        }
+
+        private Employee GetEmployee()
+        {
+            return dgvEmployees.CurrentRow.DataBoundItem as Employee;
+        }
+
+        private void btnAddEmployee_Click(object sender, EventArgs e)
+        {
+            using (var context = new EF_DBEntities())
+            {
+                string fname = tbFirstName.Text;
+                string lname = tbLastName.Text;
+
+                Employee employee = new Employee()
+                {
+                    FirstName = fname,
+                    LastName = lname
+                };
+                context.Employees.Add(employee);
+                context.SaveChanges();
+            }
+           dgvEmployees.DataSource = GetEmployees();
+           tbFirstName.Clear();
+           tbLastName.Clear(); 
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            using (var context = new EF_DBEntities())
+            {
+                string description = tbDescription.Text;
+                int priority = int.Parse(cmbPriority.SelectedItem.ToString());
+                Employee employee = GetEmployee();
+                context.Employees.Attach(employee);
+                Task task = new Task()
+                {
+                    Description = description,
+                    Priority = priority,
+                    EmployeeId = employee.Id
+                };
+                context.Tasks.Add(task);
+                context.SaveChanges();
+
+            }
+            tbDescription.Clear();
+            
+            Osvjezi();
+        }
+
+        private void btnDeleteTask_Click(object sender, EventArgs e)
+        {
+           
+            using (var context = new EF_DBEntities())
+            {
+                Task task = dgvTasks.CurrentRow.DataBoundItem as Task;
+                context.Tasks.Attach(task);
+                context.Tasks.Remove(task);
+                context.SaveChanges();
+            }
+            Osvjezi();
+        }
+    }
+}
