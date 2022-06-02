aa046abb2d1a24ff56f62208e4e2dd720ad960e1
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-03 00:28:55 +0200

    Add files via upload

---
+++ Osnove LINQ-a/Prijevoznici i kupci/OrderINFO.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace _01_prijevoznici_i_kupci
+{
+    partial class OrderINFOFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbShipper = new System.Windows.Forms.ComboBox();
+            this.cmbCustomer = new System.Windows.Forms.ComboBox();
+            this.btnFiltriraj = new System.Windows.Forms.Button();
+            this.btnPrikažiSve = new System.Windows.Forms.Button();
+            this.dgvOrders = new System.Windows.Forms.DataGridView();
+            this.dgvOrderDetail = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shipper:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 34);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Customer";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 198);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Order details: ";
+            // 
+            // cmbShipper
+            // 
+            this.cmbShipper.FormattingEnabled = true;
+            this.cmbShipper.Location = new System.Drawing.Point(107, 28);
+            this.cmbShipper.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbShipper.Name = "cmbShipper";
+            this.cmbShipper.Size = new System.Drawing.Size(92, 21);
+            this.cmbShipper.TabIndex = 4;
+            // 
+            // cmbCustomer
+            // 
+            this.cmbCustomer.FormattingEnabled = true;
+            this.cmbCustomer.Location = new System.Drawing.Point(294, 28);
+            this.cmbCustomer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbCustomer.Name = "cmbCustomer";
+            this.cmbCustomer.Size = new System.Drawing.Size(108, 21);
+            this.cmbCustomer.TabIndex = 5;
+            // 
+            // btnFiltriraj
+            // 
+            this.btnFiltriraj.Location = new System.Drawing.Point(422, 28);
+            this.btnFiltriraj.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnFiltriraj.Name = "btnFiltriraj";
+            this.btnFiltriraj.Size = new System.Drawing.Size(56, 19);
+            this.btnFiltriraj.TabIndex = 6;
+            this.btnFiltriraj.Text = "Filtriraj";
+            this.btnFiltriraj.UseVisualStyleBackColor = true;
+            this.btnFiltriraj.Click += new System.EventHandler(this.btnFiltriraj_Click);
+            // 
+            // btnPrikažiSve
+            // 
+            this.btnPrikažiSve.Location = new System.Drawing.Point(503, 28);
+            this.btnPrikažiSve.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPrikažiSve.Name = "btnPrikažiSve";
+            this.btnPrikažiSve.Size = new System.Drawing.Size(76, 19);
+            this.btnPrikažiSve.TabIndex = 7;
+            this.btnPrikažiSve.Text = "Prikaži sve";
+            this.btnPrikažiSve.UseVisualStyleBackColor = true;
+            this.btnPrikažiSve.Click += new System.EventHandler(this.btnPrikažiSve_Click);
+            // 
+            // dgvOrders
+            // 
+            this.dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrders.Location = new System.Drawing.Point(107, 57);
+            this.dgvOrders.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvOrders.Name = "dgvOrders";
+            this.dgvOrders.RowHeadersWidth = 51;
+            this.dgvOrders.RowTemplate.Height = 24;
+            this.dgvOrders.Size = new System.Drawing.Size(472, 122);
+            this.dgvOrders.TabIndex = 8;
+            this.dgvOrders.SelectionChanged += new System.EventHandler(this.dgvOrders_SelectionChanged);
+            // 
+            // dgvOrderDetail
+            // 
+            this.dgvOrderDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrderDetail.Location = new System.Drawing.Point(107, 223);
+            this.dgvOrderDetail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvOrderDetail.Name = "dgvOrderDetail";
+            this.dgvOrderDetail.RowHeadersWidth = 51;
+            this.dgvOrderDetail.RowTemplate.Height = 24;
+            this.dgvOrderDetail.Size = new System.Drawing.Size(472, 122);
+            this.dgvOrderDetail.TabIndex = 9;
+            // 
+            // OrderINFOFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.dgvOrderDetail);
+            this.Controls.Add(this.dgvOrders);
+            this.Controls.Add(this.btnPrikažiSve);
+            this.Controls.Add(this.btnFiltriraj);
+            this.Controls.Add(this.cmbCustomer);
+            this.Controls.Add(this.cmbShipper);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "OrderINFOFrm";
+            this.Text = "Order INFO";
+            this.Load += new System.EventHandler(this.OrderINFOFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbShipper;
+        private System.Windows.Forms.ComboBox cmbCustomer;
+        private System.Windows.Forms.Button btnFiltriraj;
+        private System.Windows.Forms.Button btnPrikažiSve;
+        private System.Windows.Forms.DataGridView dgvOrders;
+        private System.Windows.Forms.DataGridView dgvOrderDetail;
+    }
+}
+
---
+++ Osnove LINQ-a/Prijevoznici i kupci/OrderINFO.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_prijevoznici_i_kupci
+{
+    public partial class OrderINFOFrm : Form
+    {
+        public OrderINFOFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderINFOFrm_Load(object sender, EventArgs e)
+        {
+            PopuniKupce();
+            PopuniDobavljace();
+        }
+
+        private void DohvatiSveDetalje()
+        {
+           using (var context = new NorthwindEntities())
+            {
+                var query = from o in context.Order_Details.Include("Order")
+                            select o;
+                dgvOrderDetail.DataSource = query.ToList();
+            }
+        }
+
+
+        private void PopuniDobavljace()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var query = from s in context.Shippers
+                            select s;
+
+                cmbShipper.DataSource = query.ToList();
+
+            }
+        }
+
+        private void PopuniKupce()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var query = from k in context.Customers
+                            select k;
+
+                cmbCustomer.DataSource = query.ToList();
+
+            }
+        }
+
+        private void DohvatiNarudzbe()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var query = from k in context.Orders.Include("Customer").Include("Shipper").Include("Order_Details")
+                            select k;
+                
+                dgvOrders.DataSource = query.ToList();
+            }
+        }
+
+        private void btnPrikažiSve_Click(object sender, EventArgs e)
+        {
+            DohvatiNarudzbe();
+        }
+
+        private void btnFiltriraj_Click(object sender, EventArgs e)
+        {
+            Customer customer = cmbCustomer.SelectedItem as Customer;
+            Shipper shipper = cmbShipper.SelectedItem as Shipper;
+
+            using(var context = new NorthwindEntities())
+            {
+                var query = from k in context.Orders.Include("Customer").Include("Shipper").Include("Order_Details")
+                            where k.CustomerID == customer.CustomerID && k.ShipVia == shipper.ShipperID
+                            select k;
+
+                dgvOrders.DataSource = query.ToList();
+            }
+        }
+
+        private void dgvOrders_SelectionChanged(object sender, EventArgs e)
+        {
+            Order order = dgvOrders.CurrentRow.DataBoundItem as Order;
+            using (var context = new NorthwindEntities())
+            {
+                var query = from o in context.Order_Details
+                            where o.OrderID == order.OrderID
+                            select o;
+
+                dgvOrderDetail.DataSource = query.ToList();
+                dgvOrderDetail.Columns["Order"].Visible = false;
+            }
+        }
+    }
+}
