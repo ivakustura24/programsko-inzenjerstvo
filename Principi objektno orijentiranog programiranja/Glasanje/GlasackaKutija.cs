0fb77bd03382113fd30dbb5b4d5a6fa6a0986320
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-03 00:50:54 +0200

    Add files via upload

---
+++ Principi objektno orijentiranog programiranja/Glasanje/GlasackaKutija.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glasanje
+{
+    internal class GlasackaKutija
+    {
+        private List<Glas> Glasovi=new List<Glas>();
+    
+        private bool VecGlasao (string oib)
+        {
+            foreach(Glas g in Glasovi)
+            {
+                if (g.OIB == oib)
+                { 
+                    return true;
+                  
+                }
+                
+            }
+            return false;
+        }
+        public void Glasaj (string oib, string odabir)
+        {
+            
+            if (VecGlasao(oib) == false)
+            {
+                Glasovi.Add(new Glas(oib, odabir));
+                Console.WriteLine("Uspješno ste glasali!");
+            }
+            else
+                Console.WriteLine("Glasač je već glasao!");
+        }
+        public void DohvatiRezultateGlasovanja()
+        {
+            int Za=0, Protiv=0, Suzdrzan = 0;
+            foreach (Glas g in Glasovi)
+            {
+                if (g.Odabir=="Z")
+                    Za ++;
+                else if (g.Odabir =="P")
+                    Protiv ++;
+                else
+                    Suzdrzan ++;
+            }
+            
+            Console.WriteLine($"Rezultati glasanja: ZA = {Za}, PROTIV = {Protiv}, SUZDRŽAN = {Suzdrzan}. ");
+        }
+        
+    }
+}
