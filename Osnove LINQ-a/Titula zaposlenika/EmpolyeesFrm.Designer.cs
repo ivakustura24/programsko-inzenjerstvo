ff139ded13f66836a1b19fdf99b3e97911a284b7
Iva Kustura <101880426+ivakustura24@users.noreply.github.com>
2022-06-03 00:30:53 +0200

    Add files via upload

---
+++ Osnove LINQ-a/Titula zaposlenika/EmpolyeesFrm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace _04_titula_zaposlenika
+{
+    partial class EmployeesFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbTitle = new System.Windows.Forms.ComboBox();
+            this.TbName = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ShowAll = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Location = new System.Drawing.Point(28, 83);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.RowHeadersWidth = 51;
+            this.dgvEmployees.RowTemplate.Height = 24;
+            this.dgvEmployees.Size = new System.Drawing.Size(869, 293);
+            this.dgvEmployees.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employees";
+            // 
+            // cmbTitle
+            // 
+            this.cmbTitle.FormattingEnabled = true;
+            this.cmbTitle.Location = new System.Drawing.Point(329, 41);
+            this.cmbTitle.Name = "cmbTitle";
+            this.cmbTitle.Size = new System.Drawing.Size(173, 24);
+            this.cmbTitle.TabIndex = 2;
+            // 
+            // TbName
+            // 
+            this.TbName.Location = new System.Drawing.Point(194, 395);
+            this.TbName.Name = "TbName";
+            this.TbName.Size = new System.Drawing.Size(126, 22);
+            this.TbName.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(346, 394);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(101, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 401);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "FirstName or LastName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Title";
+            // 
+            // ShowAll
+            // 
+            this.ShowAll.Location = new System.Drawing.Point(789, 40);
+            this.ShowAll.Name = "ShowAll";
+            this.ShowAll.Size = new System.Drawing.Size(101, 23);
+            this.ShowAll.TabIndex = 7;
+            this.ShowAll.Text = "Show all";
+            this.ShowAll.UseVisualStyleBackColor = true;
+            this.ShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(667, 41);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(101, 23);
+            this.btnFilter.TabIndex = 8;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // EmployeesFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 450);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.ShowAll);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.TbName);
+            this.Controls.Add(this.cmbTitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvEmployees);
+            this.Name = "EmployeesFrm";
+            this.Text = "Employees";
+            this.Load += new System.EventHandler(this.EmployeesFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEmployees;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbTitle;
+        private System.Windows.Forms.TextBox TbName;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ShowAll;
+        private System.Windows.Forms.Button btnFilter;
+    }
+}
+
---
+++ Osnove LINQ-a/Titula zaposlenika/EmpolyeesFrm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_titula_zaposlenika
+{
+    public partial class EmployeesFrm : Form
+    {
+        public EmployeesFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            ShowAllF();
+        }
+
+        private void ShowAllF()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var query = from e in context.Employees
+                            select new { e.FirstName, e.LastName, e.Title, e.City, e.Country };
+                dgvEmployees.DataSource = query.ToList();
+
+            }
+        }
+
+        private void EmployeesFrm_Load(object sender, EventArgs e)
+        {
+            ShowAllF();
+            FillTitles();
+        }
+
+        private void FillTitles()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var query = from e in context.Employees
+                            select e.Title.ToString();
+
+                cmbTitle.DataSource = query.Distinct().ToList();
+            }
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            var title = cmbTitle.SelectedItem as string;
+            using (var context = new NorthwindEntities())
+            {
+                var query = from f in context.Employees
+                            where title == f.Title
+                            select new
+                            {
+                                f.FirstName,
+                                f.LastName,
+                                f.Title,
+                                f.City,
+                                f.Country
+                            };
+
+                dgvEmployees.DataSource = query.ToList();
+
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string find = TbName.Text.ToString();
+
+            using (var context = new NorthwindEntities())
+            {
+                var query = from l in context.Employees
+                            where l.FirstName.Contains (find) || l.LastName.Contains (find) 
+                            select new
+                            {
+                                l.FirstName,
+                                l.LastName,
+                                l.Title,
+                                l.City,
+                                l.Country
+                            };
+
+                dgvEmployees.DataSource = query.ToList();
+
+            }
+        }
+    }
+}
